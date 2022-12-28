4ad6b4fd82c7528b458ffb19e23f34b341dc3d41
Juan Verhook <juanfranciscoverhook@gmail.com>
2022-12-28 04:35:19 -0600

    Code refactoring and polishing

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages   
 
 setup(
-    name='LOTR3-demo', version='0.1', license='MIT',     
+    name='LOTRSDKTEST', version='1.0', license='MIT',     
     author="Juan Francisco Verhook",     author_email='juanfranciscoverhook@gmail.com',     
     packages=find_packages('src'),     package_dir={'': 'src'},     
     url='https://github.com/Juanvulcano/Juan_Verhook-SDK',     
--- src/LOTRSDKTEST/example.py
+++ src/LOTRSDKTEST/example.py
@@ -1,4 +1,4 @@
-from client import API_Call
+from LOTRSDKTEST.client import API_Call
 client = API_Call("https://the-one-api.dev/", "/v2/book/", "GET")
 response = client.api_request()
 all_books = client.serialize_book_names()
